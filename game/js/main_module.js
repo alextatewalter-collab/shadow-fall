5ff029cac06e453cafce90f48cd1b8f9b039ab32
alextatewalter-collab <alextatewalter@gmail.com>
2025-10-25 18:29:00 +0100

    Update main_module.js

--- game/js/main_module.js
+++ game/js/main_module.js
@@ -1,6 +1,5 @@
-
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.156.0/build/three.module.js';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.156.0/examples/jsm/controls/OrbitControls.js';
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.156.1/build/three.module.js";
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.156.1/examples/jsm/controls/OrbitControls.js";
 
 window.addEventListener('DOMContentLoaded', () => {
   const bgCanvas = document.getElementById('bgCanvas'); const bctx = bgCanvas.getContext('2d');
